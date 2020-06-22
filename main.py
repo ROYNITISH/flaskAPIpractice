418057d1c36a3497cb570fc7480ac0a239a486b4
Nitish Roy <nitishroy067@gmail.com>
2020-06-22 10:16:29 +0000

    added a not found route

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from flask import Flask,jsonify
 app = Flask(__name__)
 @app.route('/')
 def hello():
-  return jsonify(message="HelloWorld")
+  return jsonify(message="HelloWorld"),200
+@app.route('not_found')
+def not_found():
+  return jsonify(message="page not found"),404
 if __name__=="__main__":
   app.run(host="0.0.0.0",debug=True)
