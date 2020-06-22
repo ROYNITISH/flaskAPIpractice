d84c4d158d932e845d4df9ac87f6d03ac0232937
Nitish Roy <nitishroy067@gmail.com>
2020-06-22 10:37:25 +0000

    added parameter route

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask,jsonify
+from flask import Flask,jsonify,request
 app = Flask(__name__)
 @app.route('/')
 def hello():
@@ -7,5 +7,14 @@ def hello():
 @app.route('/not_found')
 def not_found():
   return jsonify(message="route not found"),404
+
+
+@app.route('/parameters')
+def parameters():
+  name = request.args.get('name')
+  age = int(request.args.get('age'))
+  if age  < 18:
+    return jsonify(message="Sorry "+ name + " you are not old enough"),401
+
 if __name__=="__main__":
   app.run(host="0.0.0.0",debug=True)
